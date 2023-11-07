5913ff308f8d827a2e50aa686113fd1003d12fa2
mina <minaduranovic@gmail.com>
2023-11-07 14:55:27 +0100

    xml gotovo

--- src/main/java/ba/unsa/etf/rpr/LaptopDaoXMLFile.java
+++ src/main/java/ba/unsa/etf/rpr/LaptopDaoXMLFile.java
@@ -1,9 +1,64 @@
 package ba.unsa.etf.rpr;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class LaptopDaoXMLFile {
+public class LaptopDaoXMLFile implements LaptopDao {
     File file;
     ArrayList<Laptop> laptopi;
+    public LaptopDaoXMLFile(File file) {
+        this.file = file;
+        this.laptopi = new ArrayList<>();
+    }
+    @Override
+    public void dodajLaptopUListu(Laptop laptop) {
+     laptopi.add(laptop);
+    }
+
+    @Override
+    public void dodajLaptopUFile(Laptop laptop) {
+        ArrayList<Laptop> postojeci = vratiPodatkeIzDatoteke();
+
+        postojeci.add(laptop);
+
+        try {
+            XmlMapper xmlMapper = new XmlMapper();
+            xmlMapper.configure(ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true);
+            xmlMapper.writeValue(file, postojeci);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Laptop getLaptop(String procesor) {
+        for (Laptop laptop : laptopi) {
+            if (laptop.getProcesor().equals(procesor)) {
+                return laptop;
+            }
+        }
+        throw new NeodgovarajuciProcesorException("Nema laptopa sa procesorom: " + procesor);
+    }
+
+    @Override
+    public void napuniListu(ArrayList<Laptop> laptopi) {
+        this.laptopi = laptopi;
+    }
+
+    @Override
+    public ArrayList<Laptop> vratiPodatkeIzDatoteke() {
+        if (file.exists()) {
+            try {
+                XmlMapper xmlMapper = new XmlMapper();
+                return xmlMapper.readValue(file, xmlMapper.getTypeFactory().constructCollectionType(ArrayList.class, Laptop.class));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new ArrayList<>();
+    }
 }
