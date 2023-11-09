5e4b8e7a0208d7dcc369b35ad821016d964f5746
mina <minaduranovic@gmail.com>
2023-11-09 23:34:31 +0100

    setUp

---
+++ test/ba/unsa/etf/rpr/LaptopDaoXMLFileTest.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaptopDaoXMLFileTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void dodajLaptopUListu() {
+    }
+
+    @Test
+    void dodajLaptopUFile() {
+    }
+
+    @Test
+    void getLaptop() {
+    }
+
+    @Test
+    void napuniListu() {
+    }
+
+    @Test
+    void vratiPodatkeIzDatoteke() {
+    }
+}
