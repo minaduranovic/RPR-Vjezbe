59311a84f6b9e4596e0f7da71ca9958cea29c179
mina <minaduranovic@gmail.com>
2023-11-10 20:47:51 +0100

    txt

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args){
 
 
-                LaptopDaoSerializableFile serializableFileDao = new LaptopDaoSerializableFile(new File("laptopi.ser"));
+                LaptopDaoSerializableFile serializableFileDao = new LaptopDaoSerializableFile(new File("laptopi.txt"));
                 LaptopDaoJSONFile jsonFileDao = new LaptopDaoJSONFile(new File("laptopi.json"));
                 LaptopDaoXMLFile xmlFileDao = new LaptopDaoXMLFile(new File("laptopi.xml"));
-
+//
                 serializableFileDao.dodajLaptopUListu(new Laptop("Dell", "XPS 13", 1500.0, 16, 512, 0, "Intel i7", "Integrated", 13.3));
                 jsonFileDao.dodajLaptopUListu(new Laptop("HP", "Pavilion", 800.0, 8, 1000, 256, "AMD Ryzen 5", "NVIDIA GTX 1650", 15.6));
                 xmlFileDao.dodajLaptopUListu(new Laptop("Apple", "MacBook Pro", 2000.0, 32, 512, 0, "Apple M1", "Apple M1", 13.3));
