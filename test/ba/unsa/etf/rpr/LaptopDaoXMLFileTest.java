cb607c11a1250be4a8ea961fda2792f08446d675
mina <minaduranovic@gmail.com>
2023-11-13 22:43:25 +0100

    ne prolazi za vratiPodatke i dodajUFile

--- test/ba/unsa/etf/rpr/LaptopDaoXMLFileTest.java
+++ test/ba/unsa/etf/rpr/LaptopDaoXMLFileTest.java
@@ -1,104 +1,61 @@
 package ba.unsa.etf.rpr;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedConstruction;
-
 import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class LaptopDaoXMLFileTest {
 
-    private LaptopDaoXMLFile laptopDao;
-    private File file;
-
-    @BeforeEach
-    void setUp() {
-        // Postavite file pre svakog testa
-        file = new File("test.xml");
-        laptopDao = new LaptopDaoXMLFile(file);
-    }
-
-    @AfterEach
-    void tearDown() {
-        // Očistite resurse, npr. izbrišite datoteku
-        if (file.exists()) {
-            file.delete();
-        }
-    }
-
     @Test
-    void dodajLaptopUListu() {
-        Laptop laptop = new Laptop("Brand", "Model", 1000.0, 8, 512, 256, "Procesor", "Graficka", 15.6);
+    void dodajLaptopUListuTest() {
+        LaptopDaoXMLFile laptopDao = new LaptopDaoXMLFile(new File("test.xml"));
+        Laptop laptop = new Laptop("Brand1", "Model1", 1000.0, 8, 500, 256, "Procesor1", "Graficka1", 15.6);
+
         laptopDao.dodajLaptopUListu(laptop);
         assertEquals(1, laptopDao.laptopi.size());
         assertEquals(laptop, laptopDao.laptopi.get(0));
     }
-    //// nisu dobre metode dodajLapotopUFile i vratiPodatkeIzDatoteke
-    @Test
-    void dodajLaptopUFile() throws IOException, NoSuchFieldException, IllegalAccessException {
-        Laptop laptop = new Laptop("Brand", "Model", 1000.0, 8, 512, 256, "Procesor", "Graficka", 15.6);
-
-        // Mock XmlMapper
-        XmlMapper xmlMapperMock = mock(XmlMapper.class);
-        doAnswer(invocation -> {
-            // Ovde možete dodati kod koji treba izvršiti kada se pozove writeValue
-            return null;
-        }).when(xmlMapperMock).writeValue(any(File.class), any(ArrayList.class));
-
-        // Korišćenje refleksije da postavimo mock objekat kao zamenu
-        Field field = LaptopDaoXMLFile.class.getDeclaredField("xmlMapper");
-        field.setAccessible(true);
-        field.set(laptopDao, xmlMapperMock);
-
-        try {
-            laptopDao.dodajLaptopUFile(laptop);
-        } finally {
-            // Vratite stvarni XmlMapper nakon izvršavanja testa
-            field.set(laptopDao, new XmlMapper());
-        }
-
-        // Provera da li je metoda writeValue pozvana
-        verify(xmlMapperMock).writeValue(eq(file), any(ArrayList.class));
-    }
-
 
     @Test
-    void getLaptop() {
-        Laptop laptop = new Laptop("Brand", "Model", 1000.0, 8, 512, 256, "Procesor", "Graficka", 15.6);
-        laptopDao.laptopi.add(laptop);
+    void dodajLaptopUFileTest() {
+        File testFile = new File("test.xml");
+        LaptopDaoXMLFile laptopDao = new LaptopDaoXMLFile(testFile);
+        Laptop laptop = new Laptop("Brand1", "Model1", 1000.0, 8, 500, 256, "Procesor1", "Graficka1", 15.6);
 
-        assertEquals(laptop, laptopDao.getLaptop("Procesor"));
+        laptopDao.dodajLaptopUFile(laptop);
+        assertTrue(testFile.exists());
+
+        // Dodatni testovi moguće su provere da li su podaci zapravo upisani u datoteku
     }
 
     @Test
-    void napuniListu() {
+    void getLaptopTest() {
+        LaptopDaoXMLFile laptopDao = new LaptopDaoXMLFile(new File("test.xml"));
+        Laptop laptop = new Laptop("Brand1", "Model1", 1000.0, 8, 500, 256, "Procesor1", "Graficka1", 15.6);
+        laptopDao.dodajLaptopUListu(laptop);
+
+        assertEquals(laptop, laptopDao.getLaptop("Procesor1"));
+    }
+
+    @Test
+    void napuniListuTest() {
+        LaptopDaoXMLFile laptopDao = new LaptopDaoXMLFile(new File("test.xml"));
         ArrayList<Laptop> lista = new ArrayList<>();
-        lista.add(new Laptop("Brand", "Model", 1000.0, 8, 512, 256, "Procesor", "Graficka", 15.6));
-        laptopDao.napuniListu(lista);
+        lista.add(new Laptop("Brand1", "Model1", 1000.0, 8, 500, 256, "Procesor1", "Graficka1", 15.6));
 
+        laptopDao.napuniListu(lista);
         assertEquals(lista, laptopDao.laptopi);
     }
+
     @Test
-    void vratiPodatkeIzDatoteke() {
+    void vratiPodatkeIzDatotekeTest() {
+        LaptopDaoXMLFile laptopDao = new LaptopDaoXMLFile(new File("test.xml"));
         ArrayList<Laptop> lista = new ArrayList<>();
-        lista.add(new Laptop("Brand", "Model", 1000.0, 8, 512, 256, "Procesor", "Graficka", 15.6));
+        lista.add(new Laptop("Brand1", "Model1", 1000.0, 8, 500, 256, "Procesor1", "Graficka1", 15.6));
+        laptopDao.napuniListu(lista);
 
-        // Mock konstrukciju XmlMapper
-        try (MockedConstruction<XmlMapper> mockedConstruction = mockConstruction(XmlMapper.class, (mock, context) -> {
-            when(mock.readValue(eq(file), any(TypeReference.class))).thenReturn(lista);
-        })) {
-            ArrayList<Laptop> rezultat = laptopDao.vratiPodatkeIzDatoteke();
-            assertEquals(lista, rezultat);
-        }
+        ArrayList<Laptop> listaIzDatoteke = laptopDao.vratiPodatkeIzDatoteke();
+        assertEquals(lista, listaIzDatoteke);
     }
-
 }
